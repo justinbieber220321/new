f9de99bbaa15d0689879f3a751dd2686cefca36b
root <root@vultr.guest>
2021-07-06 03:40:41 +0000

    change data

--- app/Http/Controllers/Frontend/DashboardController.php
+++ app/Http/Controllers/Frontend/DashboardController.php
@@ -11,23 +11,23 @@ class DashboardController extends FrontendController
     {
         $user = frontendCurrentUser();
 
-        $countUserDirect = User::delFlagOn()->statusOn()->where('player_code', $user->user_id)->count();
-        $countUserTmp = userAllChildsIds($user);
+//        $countUserDirect = User::delFlagOn()->statusOn()->where('player_code', $user->user_id)->count();
+  //      $countUserTmp = userAllChildsIds($user);
 
-        $listUserActive = User::delFlagOn()->statusOn()->whereIn('user_id', $countUserTmp)->get();
+//        $listUserActive = User::delFlagOn()->statusOn()->whereIn('user_id', $countUserTmp)->get();
 
-        $countUser = 0;
-        $dataApi = getDataApi();
-        foreach ($listUserActive as $item) {
-            $getBet = arrayGet(getBet($item, $dataApi), 'myBet') - $item->number_bet_old;
-            if ($getBet >= 100) {
-                $countUser++;
-            }
-        }
+  //      $countUser = 0;
+    //    $dataApi = getDataApi();
+      //  foreach ($listUserActive as $item) {
+        //    $getBet = arrayGet(getBet($item, $dataApi), 'myBet') - $item->number_bet_old;
+          //  if ($getBet >= 100) {
+            //    $countUser++;
+           // }
+       // }
 
         $viewData = [
-            'countUserDirect' => $countUserDirect,
-            'countUser' => $countUser
+            'countUserDirect' => 0,
+            'countUser' => 0
         ];
 
         return view('frontend.dashboard.index', $viewData);
